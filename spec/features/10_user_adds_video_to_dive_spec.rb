eb32ca2899a468e87ce547fae7d844de40f5de4a
Alex Barstow <alexander.barstow@gmail.com>
2016-10-21 17:18:10 -0400

    video test green

--- spec/features/10_user_adds_video_to_dive_spec.rb
+++ spec/features/10_user_adds_video_to_dive_spec.rb
@@ -1,25 +1,25 @@
 require 'rails_helper'
 
-feature 'user uploads photo to dive', %(
+feature 'user adds a video to dive', %(
   As an authenticated user
-  I want to be able to add photos to a dive
-  So that I can remember what the dive was like
+  I want to be able to add videos to a dive
+  So that I can remember and share what the dive was like
 ) do
 
   let!(:user) { FactoryGirl.create(:user) }
   let!(:dive) { FactoryGirl.create(:dive, user: user) }
 
-  scenario 'user can add a video by submitting its url' do
+  scenario 'user can add a video by submitting its url', js: true do
     visit root_path
     click_link 'Sign In'
     fill_in 'Email', with: user.email
     fill_in 'user_password', with: user.password
     click_button 'Sign In'
     click_link 'View Details'
-    fill_in 
-  end
+    fill_in 'video-url', with: 'https://www.youtube.com/watch?v=U-VCFG8yJ5w'
+    click_button 'Submit'
+    visit dive_path(dive)
 
-  scenario '' do
-
+    expect(page).to have_css("iframe[src*='https://www.youtube.com/embed/U-VCFG8yJ5w']")
   end
 end
