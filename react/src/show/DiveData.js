ad8d031c3c6b437ae8500cb44a4a6386fe9383ea
Alex Barstow <alexander.barstow@gmail.com>
2016-10-21 12:18:01 -0400

    fixed delete route on show page

--- react/src/show/DiveData.js
+++ react/src/show/DiveData.js
@@ -6,7 +6,7 @@ const DiveData = props => {
     <div>
       <h1>{props.data.title}</h1>
       <a href={`/dives/${props.data.id}/edit`} className="button radius secondary home-button">Edit</a>
-      <a href={`/dives/${props.data.id}/edit`} className="button radius alert home-button" data-method="delete">Delete</a>
+      <a href={`/dives/${props.data.id}`} className="button radius alert home-button" data-method="delete">Delete</a>
       <a href='/dives'>Back</a>
       <p>{props.data.site_name}</p>
       <p>{props.data.max_depth} ft</p>
