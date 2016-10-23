5ea59dd1ecc8a58f6af1b4299a9c16d0868b68fd
Alex Barstow <alexander.barstow@gmail.com>
2016-10-23 18:57:52 -0400

    hound edits

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,7 @@ class HomeController < ApplicationController
   def index
     url = request.original_url
     @path = URI(url).path
-    
-    if current_user
-      redirect_to dives_path
-    end
+
+    redirect_to dives_path if current_user
   end
 end
