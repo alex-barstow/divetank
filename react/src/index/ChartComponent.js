7ecd1b9cc49b60ef7cec76ef9d2e6d4e2ecc84b4
Alex Barstow <alexander.barstow@gmail.com>
2016-10-28 11:29:08 -0400

    hound fixes

--- react/src/index/ChartComponent.js
+++ react/src/index/ChartComponent.js
@@ -11,17 +11,17 @@ const ChartComponent = props => {
   let fifthRange = props.columnChart.fifth;
 
   let lineChartData = props.dives.map(dive => {
-    let month = dive.date.split("/")[0]
-    let day = dive.date.split("/")[1]
-    let year = dive.date.split("/")[2]
+    let month = dive.date.split("/")[0];
+    let day = dive.date.split("/")[1];
+    let year = dive.date.split("/")[2];
 
-    let dateString = `${year}-${month}-${day}`
+    let dateString = `${year}-${month}-${day}`;
     let depth = dive.max_depth;
 
     return (
       [new Date(dateString), depth]
-    )
-  })
+    );
+  });
 
   return (
     <div>
